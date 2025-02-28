53cd2cb17b1a0b0171bd329923db66634f491734
LLonghi02 <lara.longhi3@gmail.com>
2025-02-28 10:25:30 +0100

    .

--- RecSys_Porting_Project-master/run_CIKM_18_ExampleAlgorithm.py
+++ RecSys_Porting_Project-master/run_CIKM_18_ExampleAlgorithm.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
+import sys
+sys.path.append('c:/Users/laral/Desktop/Progetto RS/RS/RecSys_Porting_Project-master')
 
 from HyperparameterTuning.SearchSingleCase import SearchSingleCase
 from HyperparameterTuning.SearchAbstractClass import SearchInputRecommenderArgs
